33f4a988d428939cc1c04f83f8e4aaaeff54048b
kevit-shubham-kushwaha <shubham.kushwaha@kevit.io>
2025-05-20 11:04:02 +0530

    Adding User Routes and Restructuring Utils

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
-
+import uvicorn
+from utils.libs.config import (PORT,HOST)
+from src.routes.user import routes as user_routes
 
 app = FastAPI()
 
+app.include_router(user_routes)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+  
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host=HOST, port=PORT, reload=False)
