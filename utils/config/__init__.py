b0a9c3d44e4fb91b1b3857a3adbea30e8f9ee68f
kevit-shubham-kushwaha <shubham.kushwaha@kevit.io>
2025-05-20 10:16:27 +0530

    adding config file and updating README.md file

---
+++ utils/config/__init__.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGODB_URI = os.getenv("MONGODB_URI")
+MONGODB_DB = os.getenv("MONGODB_DB")
+MONGODB_COLLECTION = os.getenv("MONGODB_COLLECTION")
